0b373b72c7a705ca98b97d16c8da7b3eab800477
link <bjhhopesin@gmail.com>
2023-05-15 01:28:00 +0900

    유니티 테스트

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import Test from './pages/Test';
 
 function LoggedOutRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function Home() {
-  return <>home</>;
+  const { unityProvider } = useUnityContext({
+    loaderUrl: 'build/build.loader.js',
+    dataUrl: 'build/build.data',
+    frameworkUrl: 'build/build.framework.js',
+    codeUrl: 'build/build.wasm',
+  });
+
+  return (
+    <Box>
+      <Typography>유니티 테스트</Typography>
+      <Unity unityProvider={unityProvider} style={{ width: '100%' }} />
+    </Box>
+  );
 }
